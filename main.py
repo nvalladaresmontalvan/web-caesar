d545f01f046ace7802883b8f7bdaeef065bf7ee1
nvalladaresmontalvan <nvalladares.montalvan@gmail.com>
2018-09-25 16:04:54 -0400

    Created file  caesar.py and main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form id="caesarForm" action="/" method="post">
+           <label > Rotate By:</label>
+           <input type="text" name="rot" value=0 />
+           <textarea form="caesarForm" name="message" > </textarea>
+           <input type="submit" value="Submit Query" />
+        </form>
+    </body>
+</html>
+"""
+
+
+
+@app.route("/")
+def index():
+    return form
+
+app.run()    
